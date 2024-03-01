efa4f8383bac5add57bee67a3fa34cc3745cb5ff
akshat jaiswal <akshatjaiswal.official@gmail.com>
2024-03-02 01:52:10 +0530

    fix[code]: Changes Action return type to ReactNode.
    [fixed: Action Return Type.]

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -1,41 +1,50 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { onFollow, onUnfollow } from "@/actions/follow";
-import { ReactNode, useTransition } from "react";
 import { toast } from "sonner";
+import { useTransition } from "react";
 
-interface ActionProps {
+import { onFollow, onUnfollow } from "@/actions/follow";
+import { Button } from "@/components/ui/button";
+
+interface ActionsProps {
   isFollowing: boolean;
   userId: string;
 }
-export const Actions = ({ isFollowing, userId }: ActionProps): ReactNode => {
+
+export const Actions = ({ isFollowing, userId }: ActionsProps) => {
   const [isPending, startTransition] = useTransition();
+
   const handleFollow = () => {
     startTransition(() => {
       onFollow(userId)
         .then((data) =>
-          toast.success(`You are following ${data.following.username}`),
+          toast.success(`You are now following ${data.following.username}`),
         )
-        .catch(() => toast.error("Unable to follow the user"));
+        .catch(() => toast.error("Something went wrong"));
     });
-    const handleUnfollow = () => {
-      startTransition(() => {
-        onUnfollow(userId)
-          .then((data) =>
-            toast.success(`You Unfollowed ${data.following.username}`),
-          )
-          .catch(() => toast.error("Unable to Unfollow the user"));
-      });
-      const onClick = () => {
-        if (isFollowing) handleUnfollow();
-        else handleFollow();
-      };
-      return (
-        <Button disabled={isPending} onClick={onClick} variant={"secondary"}>
-          {isFollowing ? "Unfollow" : "Follow"}
-        </Button>
-      );
-    };
   };
+
+  const handleUnfollow = () => {
+    startTransition(() => {
+      onUnfollow(userId)
+        .then((data) =>
+          toast.success(`You have unfollowed ${data.following.username}`),
+        )
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
+  const onClick = () => {
+    if (isFollowing) {
+      handleUnfollow();
+    } else {
+      handleFollow();
+    }
+  };
+
+  return (
+    <Button disabled={isPending} onClick={onClick}>
+      {isFollowing ? "Unfollow" : "Follow"}
+    </Button>
+  );
 };
